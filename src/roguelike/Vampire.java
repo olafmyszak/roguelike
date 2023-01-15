f12bc21532077cd194da9436322b5f87cf964d37
Olaf Myszak <myszakolaf@gmail.com>
2023-01-15 00:24:11 +0000

    Delete Vampire.java

--- src/roguelike/Vampire.java
+++
@@ -1,7 +0,0 @@
-package roguelike;
-
-public class Vampire extends GameCharacter{
-    public Vampire(int length, int height, int level) {
-        super(length, height, "Vampire", "Powerful undead Count, feasting on the blood of humans. Faster and more intelligent than any normal human.", new Attribute(15), new Attribute(15), new Attribute(15), new Attribute(30), new Attribute(5), new Attribute(40), level);
-    }
-}
