e7c8043d11f3df9a8ab51f4699f3eb3dc2045e07
Olaf Myszak <myszakolaf@gmail.com>
2023-01-15 00:24:16 +0000

    Delete Ghost.java

--- src/roguelike/Ghost.java
+++
@@ -1,7 +0,0 @@
-package roguelike;
-
-public class Ghost extends GameCharacter{
-    public Ghost(int length, int height, int level) {
-        super(length, height, "Ghost", "Restless soul, stuck in the mortal realm. Cannot be harmed by standard attacks and weapons.", new Attribute(1), new Attribute(15), new Attribute(15), new Attribute(30), new Attribute(5), new Attribute(40), level);
-    }
-}
